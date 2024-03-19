43518c3d69adf6ae7734094585b9d9232ea620e7
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2024-03-19 12:18:14 -0500

    first commit with code from danieljamadotcom

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log('Start Script');
+const headerLeftBtn = document.getElementById('headerLeftBtn');
+const iconBtn1 = document.getElementById('iconBtn1');
+const iconBtn2 = document.getElementById('iconBtn2');
+const iconBtn3 = document.getElementById('iconBtn3');
+const iconBtn4 = document.getElementById('iconBtn4');
+
+const pageTurn = (pageName) => {
+	location.href = pageName;
+};
+
+const extLink = (pageURL) => {
+	window.open(pageURL, '_blank');
+};
+
+headerLeftBtn.addEventListener('click', () => {
+	pageTurn('index.html');
+});
+
+iconBtn1.addEventListener('click', () => {
+	extLink('https://www.flickr.com/jamahadrummer');
+});
+
+iconBtn2.addEventListener('click', () => {
+	extLink('https://www.instagram.com/jamahadrummer');
+});
+
+iconBtn3.addEventListener('click', () => {
+	extLink('https://jamadaniel.wixsite.com/sound-designer');
+});
+
+iconBtn4.addEventListener('click', () => {
+	extLink('https://www.github.com/jamahadrummer');
+});
+
+console.log('End Script');
