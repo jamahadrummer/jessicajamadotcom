f509bf21baf58cd91b484b999a5538456f5d8c8c
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2024-12-24 17:45:51 -0600

    basic vanilla landing update

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ headerLeftBtn.addEventListener('click', () => {
 	pageTurn('index.html');
 });
 
-// iconBtn1.addEventListener('click', () => {
-// 	extLink('https://www.flickr.com/jamahadrummer');
-// });
+iconBtn1.addEventListener('click', () => {
+	extLink('https://www.linkedin.com/in/jessica-jama-28212898/');
+});
 
 mediaBtn2.addEventListener('click', () => {
 	extLink('https://www.thegeniusofplay.org/genius/expert-advice/articles/social-emotional-learning-during-the-holidays.aspx?WebsiteKey=f24671dc-3a61-4dfa-854c-b2d74801f627');
@@ -28,9 +28,5 @@ mediaBtn2.addEventListener('click', () => {
 mediaBtn3.addEventListener('click', () => {
 	extLink('https://kidscreen.com/2023/09/29/why-self-care-and-sel-toys-are-on-the-rise/');
 });
-
-// iconBtn4.addEventListener('click', () => {
-// 	extLink('https://www.github.com/jamahadrummer');
-// });
 
 console.log('End Script');
