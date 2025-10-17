abc2505da6e1687777cc44e50d95b67cf1fdaf75
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2025-10-16 22:07:56 -0500

    finished refactoring index page for more consistency across elements, improved responsiveness for difference screen sizes

--- index.js
+++ index.js
@@ -32,16 +32,21 @@ prodBtn.pCatBtn4.addEventListener('click', () => {
 	pageTurn('products.html#mathTitle');
 });
 
-mediaBtn2.addEventListener('click', () => {
+mediaBtn1.addEventListener('click', () => {
 	extLink('https://www.thegeniusofplay.org/genius/expert-advice/articles/social-emotional-learning-during-the-holidays.aspx?WebsiteKey=f24671dc-3a61-4dfa-854c-b2d74801f627');
 });
 
-mediaBtn3.addEventListener('click', () => {
+mediaBtn2.addEventListener('click', () => {
 	extLink('https://kidscreen.com/2023/09/29/why-self-care-and-sel-toys-are-on-the-rise/');
 });
 
-mediaBtn4.addEventListener('click', () => {
+mediaBtn3.addEventListener('click', () => {
 	extLink('https://www.wbez.org/reset-with-sasha-ann-simons/2022/12/13/the-benefits-of-social-and-emotional-learning-toys');
 });
+
+sMediaBtn1.addEventListener('click', () => {
+	extLink('https://www.linkedin.com/in/jessica-jama-28212898');
+});
+
 
 console.log('End Script');
